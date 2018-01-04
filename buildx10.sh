ce55cdd2c451c5cc9264d1ff21642755ab04b7fd
Yohsuke Murase <yohsuke.murase@gmail.com>
2018-01-04 16:39:54 +0900

    clear an empty cache

--- buildx10.sh
+++ buildx10.sh
@@ -4,6 +4,7 @@ X10_RELEASE=SF_RELEASE_2_6_1
 if [ ! -d "x10/x10.dist/bin" ]; then
   wget https://github.com/x10-lang/x10/archive/$X10_RELEASE.zip -O x10src.zip
   unzip x10src.zip
+  rm -rf x10  # clear the empty dir made by travis cache
   mv x10-$X10_RELEASE x10
 fi
 if [ ! -f "x10/x10.dist/bin/x10c" ]; then

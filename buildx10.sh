2258d2f00e3ec5a30b1cac5f8ac4ea0f6399160a
Yohsuke Murase <yohsuke.murase@gmail.com>
2018-01-04 14:25:37 +0900

    skip download if x10 directory exists

--- buildx10.sh
+++ buildx10.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -eux
 
 X10_RELEASE=SF_RELEASE_2_6_1
-wget https://github.com/x10-lang/x10/archive/$X10_RELEASE.zip -O x10src.zip
-unzip x10src.zip
-mv x10-$X10_RELEASE x10
+if [ ! -d "x10" ]; then
+  wget https://github.com/x10-lang/x10/archive/$X10_RELEASE.zip -O x10src.zip
+  unzip x10src.zip
+  mv x10-$X10_RELEASE x10
+fi
 cd x10/x10.dist/
 export X10_STATIC_LIB=1
 ant -Davailable.procs=4 -Doptimize=true -DNO_CHECKS=true dist

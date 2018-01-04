3ba45ca561ec34b2c6fa9d63a144faffb9f258e3
Yohsuke Murase <yohsuke.murase@gmail.com>
2018-01-04 14:50:48 +0900

    set NPROC variable in Travis

--- buildx10.sh
+++ buildx10.sh
@@ -8,7 +8,7 @@ if [ ! -d "x10" ]; then
 fi
 cd x10/x10.dist/
 export X10_STATIC_LIB=1
-NPROC=$(getconf _NPROCESSORS_ONLN)
+NPROC=${NPROC:-$(getconf _NPROCESSORS_ONLN)}
 ant -Davailable.procs=$NPROC -Doptimize=true -DNO_CHECKS=true dist
 rm -f lib/*.so* stdlib/lib/*.so*
 cd -
